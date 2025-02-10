805cce26e06cbb74b0ce3d33ab0abe9ce08152ee
AmirMohammadAlirezaei <amirm.alirezaei@gmail.com>
2025-02-10 21:23:52 +0330

    const squares

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ for (let i = 0; i < width * width; i++) {
   const squares = document.createElement("div");
   grid.appendChild(squares);
 }
+
+const squares = Array.from(document.querySelectorAll(".grid div"));
