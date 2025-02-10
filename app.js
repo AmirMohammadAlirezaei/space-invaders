861cfa22288b64d2328de13fc0d0fb87c2cebe4f
AmirMohammadAlirezaei <amirm.alirezaei@gmail.com>
2025-02-10 21:29:16 +0330

    alienIvaders[]

--- app.js
+++ app.js
@@ -8,3 +8,51 @@ for (let i = 0; i < width * width; i++) {
 }
 
 const squares = Array.from(document.querySelectorAll(".grid div"));
+console.log(squares);
+
+const alienInvaders = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21,
+  22,
+  ,
+  23,
+  24,
+  25,
+  26,
+  27,
+  28,
+  2930,
+  31,
+  32,
+  33,
+  34,
+  35,
+  36,
+  37,
+  38,
+  39,
+];
+
+function draw() {
+  for (let i = 0; i < alienInvaders.length; i++) {}
+}
