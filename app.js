3c8c66ab22241b9293706796eb8467abe9081429
AmirMohammadAlirezaei <amirm.alirezaei@gmail.com>
2025-02-10 21:16:35 +0330

    fore squares

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const grid = document.querySelector(".grid");
 const resultDisplay = document.querySelector(".results");
+const width = 15;
+
+for (let i = 0; i < width * width; i++) {
+  const squares = document.createElement("div");
+}
